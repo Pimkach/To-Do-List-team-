6e8a3c029aba0385167fe68b909301d793104ccd
Zebroo43 <zebromarin@mail.ru>
2024-08-19 18:47:32 +0300

    commit

--- interface.py
+++ interface.py
@@ -1,2 +1,16 @@
-hello
-hello
+print ("Добро пожаловать!")
+
+vubor = None
+while vubor not in ['1', '2','3']:
+    vubor = input("""Что сегодня будем делать?
+    1.Добавить задачу
+    2.Просмотр задачи
+    3.Удаление задачи.
+    Ввод: """)
+    if vubor == "1":
+        print("работает")
+    if vubor == "2":
+        print("работает")
+    if vubor == "3":
+        print('работает')
+input()
