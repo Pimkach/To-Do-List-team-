9b6d1a25a1a43fdc86c66001476bef502533ecde
Pimkach <kososes@gmail.com>
2024-08-20 12:47:40 +0600

    remove_task

--- tasks.py
+++ tasks.py
@@ -6,3 +6,12 @@ from todo import task_list # список для хранения задач
 def add_task(task_description):
     task_list.append({"описание": task_description, "статус": False})
     print(f"Задача '{task_description}' успешно добавлена!")
+
+def remove_task(number):
+    index = number - 1  # Преобразуем номер задачи в индекс
+
+    if 0 <= index < len(task_list):
+        removed_task = task_list.pop(index)
+        print(f"Задача '{removed_task}' удалена.")
+    else:
+        print("Неверный номер задачи. Задача не найдена.")
