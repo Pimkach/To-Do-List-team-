0600b3a6f6b21e6252f6a598606e01171c788f75
Zebroo43 <zebromarin@mail.ru>
2024-08-22 16:37:33 +0300

    commit1

--- storage.py
+++ storage.py
@@ -1,6 +1,16 @@
 
 
-
+#файл для добавления
 def save_task(task_description):
-    with open('textovick.txt', "w") as task_description:
-        print("задача сохранена", f'{task_description}',file=task_description )
+    with open('textovick.txt', "w") as file:
+        file.write("задача сохранена: " + task_description + "\n")
+
+
+#файл для чтения
+def read_task():
+    try:
+        with open('textovick.txt', "r") as file:
+            task_description = file.read()
+            print("Задача:", task_description)
+    except FileNotFoundError:
+        print("Файл не найден")
